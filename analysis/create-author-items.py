946c559a356c512ae0055b0286d3893565eb3c2f
Jakob Voss <voss@gbv.de>
2020-05-18 21:12:09 +0200

    Add option --dry

--- analysis/create-author-items.py
+++ analysis/create-author-items.py
@@ -10,25 +10,24 @@ __version__ = "2 - adaption for ORCID"
 
 
 import argparse
+import logging
 import json
 import subprocess
 import time
 from pandas import read_csv
-from pprint import pprint
 import logging
 
-logging.basicConfig(format='%(message)s')
-log = logging.getLogger(__name__)
+logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
 user_agent = "TakeItPersonally, https://github.com/foerstner-lab/TIP-lib, seidlmayer@zbmed.de"
 
 def main():
     parser = argparse.ArgumentParser(description=__description__)
     parser.add_argument("--wikidata_cli_executable", default="wd")
+    parser.add_argument("--dry", action='store_true')
     parser.add_argument("orcid_summaries_csv")
     parser.add_argument("log_file_name")
     args = parser.parse_args()
-
 
     orcid_data = read_csv(args.orcid_summaries_csv)
 
@@ -36,41 +35,53 @@ def main():
         if item_exists(row, args.wikidata_cli_executable):
             continue
         else:
-            create_new_item(row, args.wikidata_cli_executable, args.log_file_name)
-            time.sleep(3)
+            item = row_to_item(row)
+            if args.dry:
+                logging.info(json.dumps(item))
+            else:
+                create_new_item(item, args.wikidata_cli_executable, args.log_file_name)
+                time.sleep(3)
 
 
-def create_new_item(row, wikidata_cli_executable, log_file_name):
+# create json_dictionary with information for uploading to wikidata
+def row_to_item(row):
+    name = _generate_name_list(row)
+    #date = _generate_date_list(row)
+    affiliation = row['affiliation_name']
+
+    return {
+        "labels": {"en": name}, # given_name and family_name
+        "descriptions": {"en": f"researcher at {affiliation}" },
+        "claims": {
+            "P31": "Q5", # human
+            "P496": row['orcid'], # orcid
+            "P106": "Q42240", # researcher
+            "P6424": {'value': affiliation,
+                       "qualifiers": {
+                        "P580": row['affiliation_year'] } 
+                     }    # start date employment
+            #"P735": row['given_name'].title(),  # given_name
+            #"P734": row['family_name'].title(),  # family_name
+        }
+    }
+
+def create_new_item(item, wikidata_cli_executable, log_file_name):
+
     # create logging-file
-
     with open(log_file_name, 'a') as f:
-        name = _generate_name_list(row)
-        #date = _generate_date_list(row)
-
-        # create json_dictionary with information for uploading to wikidata
-        affiliation = row['affiliation_name']
-        entity_dict = {
-            "labels": {"en": name}, # given_name and family_name
-            "descriptions": {"en": f"researcher at {affiliation}" },
-            "claims": {
-                "P31": "Q5", # human
-                "P496": row['orcid'], # orcid
-                "P106": "Q42240", # researcher
-                "P6424": {'value': affiliation,
-                           "qualifiers": {
-                            "P580": row['affiliation_year'] } 
-                         }    # start date employment
-                #"P735": row['given_name'].title(),  # given_name
-                #"P734": row['family_name'].title(),  # family_name
-            }}
-        pprint(entity_dict)
+        item = row_to_item(row)
         tmp_json_file = "tmp.json"
 
         with open(tmp_json_file, "w") as entity_json_fh:
-            entity_json_fh.write(json.dumps(entity_dict))
+            entity_json_fh.write(json.dumps(item))
 
+<<<<<<< HEAD
         creation_result = subprocess.run(f"{wikidata_cli_executable} create-entity ./{tmp_json_file} --dry".split(), capture_output=True)
         print(creation_result)
+=======
+        creation_result = subprocess.run(f"{wikidata_cli_executable} create-entity ./{tmp_json_file}".split(), capture_output=True)
+        logging.info(creation_result)
+>>>>>>> Add option --dry
         if creation_result.returncode == 0:
             result = json.loads(creation_result.stdout.decode('utf-8'))
             f.write(str(result) + '\n')
@@ -123,15 +134,16 @@ def item_exists(row, wikidata_cli_executable):
                 SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en"}}
         }} }}'''
         output_fh.write(sparql_query)
-        print(sparql_query)
+        logging.debug(sparql_query)
     try:
         query_result = subprocess.check_output(
             f"{wikidata_cli_executable} sparql "
             f"{tmp_sparql_file} -e https://query.wikidata.org/sparql".split()
         )
     except subprocess.CalledProcessError:
-        log.warning("SPARQL request failed! Skipping entry...")
+        logging.warning("SPARQL request failed! Skipping entry...")
         return True
+
     # If this string is return the item is not existing
     return "no result found by name" in str(query_result)
 
