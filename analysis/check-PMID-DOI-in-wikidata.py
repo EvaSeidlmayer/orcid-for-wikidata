a248e5ea47ec37de265540e5a6a3ce056bb9b521
EvaSeidlmayer <seidlamyer@zbmed.de>
2020-06-10 13:42:36 +0200

    complete check-PMID-DOI-in wikidata.py

--- analysis/check-PMID-DOI-in-wikidata.py
+++ analysis/check-PMID-DOI-in-wikidata.py
@@ -10,11 +10,9 @@ __version__ = "1 "
 
 
 import argparse
-import time
 import pandas as pd
-from pandas import read_csv
-import logging
 from SPARQLWrapper import SPARQLWrapper, JSON
+import csv
 
 user_agent = "TakeItPersonally, https://github.com/foerstner-lab/TIP-lib, seidlmayer@zbmed.de"
 wd_url = SPARQLWrapper("https://query.wikidata.org/sparql", agent=user_agent)
@@ -27,48 +25,59 @@ def main():
     args = parser.parse_args()
 
 
-    orcid_data = read_csv(args.input_file_name)
+    orcid_data = pd.read_csv(args.input_file_name)
+    orcid_data.to_dict()
+    #print(orcid_data)
 
-    wd_check = []
+    with open(args.log_file_name, 'w') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['orcid','pmid','doi', 'qnr'])
 
-    for _, row in orcid_data.iterrows():
-        if item_exists(row, wd_url):
-            row = row['orcid'], row['pmid'], row['doi'], qnr
-            wd_check.append(row)
-        else:
-            row = row['orcid'], row['pmid'], row['doi'], ''
-            wd_check.append(row)
-            time.sleep(3)
+        try:
+            for pmid in orcid_data.values:
+                query = f'''SELECT ?item WHERE {{
+                {{ ?item wdt:P698  "{pmid[1]}" }} 
+                    ?item wdt:P31 wd:Q13442814.
+                    SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE], ar,be,bg,bn,ca,cs,da,de,el,en,es,et,fa,fi, fr,he,hi,hu,hy,id,it,ja,jv,ko,nb,nl,eo,pa,pl,pt,ro,ru,sh,sk,sr,sv,sw,te,th,tr,uk,yue,vec,vi,zh"}}
+                    }}'''
+                #print(query)
+                wd_url.setQuery(query)
+                wd_url.setReturnFormat(JSON)
+                results = wd_url.query().convert()
+                #print('pmid results:', results)
+                if (len(results['results']['bindings'])) > 0:
+                    for res in results['results']['bindings']:
+                        qnr = res['item']['value'].rsplit('/', 1)[1]
+                        print(qnr)
+                        infos = doi[0], doi[1], doi[2], qnr
+                        print('infos', infos)
+                        #print(','.join(infos))
+                        csv_writer.writerow(infos)
+        except:
+            pass
 
-    df_wd_check = pd.DataFrame(wd_check, columns=['orcid', 'pmid', 'doi', 'qnr'])
-    df_wd_check.to_csv('args.log_file_name', index=False)
+        try:
+            for doi in orcid_data.values:
+                #print(doi[2])
+                query = f'''SELECT ?item WHERE {{
+                {{ ?item wdt:P356  "{doi[2]}" }} .
+                    SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE], ar,be,bg,bn,ca,cs,da,de,el,en,es,et,fa,fi, fr,he,hi,hu,hy,id,it,ja,jv,ko,nb,nl,eo,pa,pl,pt,ro,ru,sh,sk,sr,sv,sw,te,th,tr,uk,yue,vec,vi,zh"}}
+                }}'''
+                #print(query)
+                wd_url.setQuery(query)
+                wd_url.setReturnFormat(JSON)
+                results = wd_url.query().convert()
+                #print('doi result:', results)
+                if (len(results['results']['bindings'])) > 0:
+                    for res in results['results']['bindings']:
+                        qnr = res['item']['value'].rsplit('/', 1)[1]
+                        print(qnr)
+                        infos = doi[0], doi[1], doi[2], qnr
+                        print('infos', infos)
+                        #print(','.join(infos))
+                        csv_writer.writerow(infos)
+        except:
+            pass
 
-
-def item_exists(row, wd_url):
-    """
-    Check by querying for article items with PMID and DOI
-    """
-    pmid = row['pmid']
-    doi = row['doi']
-
-    query = f'''SELECT ?item WHERE {{
-        {{ ?item wdt:P698  "{pmid}" }} OR
-        {{ ?item wdt:356 "{doi}" }} .
-            ?item wdt:P31 wd:Q13442814.
-            SERVICE wikibase:label {{ bd:serviceParam wikibase:language "[AUTO_LANGUAGE], ar,be,bg,bn,ca,cs,da,de,el,en,es,et,fa,fi, fr,he,hi,hu,hy,id,it,ja,jv,ko,nb,nl,eo,pa,pl,pt,ro,ru,sh,sk,sr,sv,sw,te,th,tr,uk,yue,vec,vi,zh"}}
-    }} }}'''
-    logging.debug(query)
-
-    wd_url.setQuery(query)
-    print(query)
-    wd_url.setReturnFormat(JSON)
-    results = wd_url.query().convert()
-    logging.debug(results)
-
-    if len(results['results']['bindings']) > 0:
-        qnr = results['results']['bindings'][0]['item']['value'].rsplit('/', 1)[1]
-    else:
-        qnr = ''
-    return qnr
 
 main()
