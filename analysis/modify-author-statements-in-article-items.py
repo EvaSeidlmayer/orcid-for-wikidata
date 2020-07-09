c355e4aa7014f78e5efc8b6dae2b5ed7fa0e635c
EvaSeidlmayer <seidlamyer@zbmed.de>
2020-07-09 16:21:07 +0200

    improve modify-author-statements-in-article-items.py

---
+++ analysis/modify-author-statements-in-article-items.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__description__ = "compares known authors from ORCID with author statements in Wikidata article item; " \
+                  "if the author statement is not complete the article item is modified apllying Wikibase CLI "
+__author__ = "Eva Seidlmayer <seidlmayer@zbmed.de>"
+__copyright__ = "2020 by Eva Seidlmayer"
+__license__ = "ISC license"
+__email__ = "seidlmayer@zbmed.de"
+__version__ = "1 "
+
+
+import argparse
+import json
+import subprocess
+from pandas import read_csv
+import logging
+import pandas as pd
+
+# create a template with article_qnr, missing author statement P50, author_qnr and name string of author
+
+def create_template_article_item(row):
+    return {
+        "id": row['article_qnr'],
+        "claims": {
+            "P50": {
+                "value": row['author_qnr'],
+                "qualifier": [{"P1932": row['given_name']}]
+            }
+        }
+    }
+
+
+# start a subprocess applying Wikibase-CLI to modify the article item using the above created template containig the missing author statement
+
+def edit_item(row, wikidata_cli_executable, log_file_name):
+    with open(log_file_name, 'a') as f:
+        item = create_template_article_item(row)
+        print(item)
+        logging.info(f'item is {item}')
+        tmp_json_file = "tmp.json"
+
+        with open(tmp_json_file, "w") as entity_json_fh:
+            entity_json_fh.write(json.dumps(item))
+
+        creation_result = subprocess.run(f"{wikidata_cli_executable} edit-entity ./{tmp_json_file} --dry".split(), capture_output=True)
+        logging.info(creation_result)
+        print(creation_result)
+        if creation_result.returncode == 0:
+            result = json.loads(creation_result.stdout.decode('utf-8'))
+            f.write(str(result) + '\n')
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__description__)
+    parser.add_argument("--wikidata_cli_executable", default="wb")
+    parser.add_argument("--dry", action='store_true')
+    parser.add_argument("--quiet", action='store_true')
+    parser.add_argument("available_articles_available_authors_csv")
+    parser.add_argument("log_file_name")
+    args = parser.parse_args()
+
+    if (args.quiet):
+        logging.basicConfig(format='%(message)s', level=logging.WARNING)
+    else:
+        logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+    wikidata_authors = read_csv(args.available_articles_available_authors_csv)
+    orcid_authors = read_csv("../available-authors-in-wd-2020-06-20.csv")
+    wikidata_authors = wikidata_authors.rename(columns={'orcid_origin' : 'orcid'})
+    all_df = pd.merge(orcid_authors, wikidata_authors, how='right', on='orcid')
+
+
+
+    # setting counters for statistical use
+    no_author = 0
+    no_all_authors = 0
+    already_registered = 0
+    needs_to_be_registered = 0
+
+    for index, row in all_df.iterrows():
+        try:
+            if pd.isna(row['author_qnr']):
+                no_author += 1
+                #print('CASE 1: author with orcid row["orcid"] needs to be registered')
+            if pd.isna(row['all_authors_qnr']):
+                no_all_authors += 1
+                #print('CASE 2: no authors in article item', row['article_qnr'], 'needs to be registered')
+
+            else:
+                if row['author_qnr'] in row['all_authors_qnr']:
+                    already_registered += 1
+                    #print('CASE 3 already registered')
+                if row['author_qnr'] == row['all_authors_qnr']:
+                    already_registered += 1
+                    #print('CASE 3: already registered')
+                else:
+                    needs_to_be_registered += 1
+                    #print('CASE 4: article-qnr:', row['article_qnr'], 'needs to be registered')
+                    edit_item(row, args.wikidata_cli_executable, args.log_file_name)
+
+        except Exception as e:
+            print(e)
+
+    print('CASE 1: authors_qnr is NaN', no_author)
+    print('CASE 2: all_authors_qnr is NaN:', no_all_authors)
+    print('CASE 3: author is in all_author_qnr', already_registered)
+    print('CASE 4: author-items exist but needed to be introduced to article_item:', needs_to_be_registered)
+
+    print('all:', len(all_df))
+    print('wikidata authors', len(wikidata_authors))
+    print('orcid authors', len(orcid_authors))
+
+
+main()
